375b73300d2caa453fb5edccd58bad48d8a9da37
Alexander <aleksander.freelancer@gmail.com>
2023-04-11 17:17:52 +0600

    After delete getUserFromData

--- php/functions/GET_functions.php
+++ php/functions/GET_functions.php
@@ -45,39 +45,3 @@ function getElementsFromData($type, $data, $connect){
 
     echo json_encode($user);
 }
-
-function getUserFromData($type, $data, $connect)
-{
-    // FIELDS
-
-    $name = $data['name'];
-    $lastName = $data['lastName'];
-    $password = $data['password'];
-
-    // VALIDATION (Checking if all fields are filled)
-
-    checkRequest([$name, $lastName, $password], "All fields are required");
-
-    // Creating a query
-
-    $sql = null;
-    if ($type == 'managers') {
-        $sql = "SELECT * FROM Managers WHERE `name` = '$name' AND `lastName` = '$lastName' AND `password` = '$password'";
-    } elseif ($type == 'movers') {
-        $sql = "SELECT * FROM movers WHERE `name` = '$name' AND `lastName` = '$lastName' AND `password` = '$password'";
-    }
-
-    $result = mysqli_query($connect, $sql);
-
-    checkDataBaseRequest($connect, $sql);
-
-    verifyNotNull($result, "User not found");
-
-    // FETCHING DATA
-
-    $people = mysqli_fetch_all($result, MYSQLI_ASSOC);
-
-
-    echo json_encode($people[0]);
-
-}
