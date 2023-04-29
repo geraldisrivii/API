8d0005c99f95b7a42e6297bdc4d36303ce61c5a3
Alexander <aleksander.freelancer@gmail.com>
2023-04-29 08:29:37 +0600

    After refactoring

--- php/functions/GET_functions.php
+++ php/functions/GET_functions.php
@@ -4,10 +4,12 @@
 function getConvertedTasks($type, $connect, $data)
 {
     $limit = $data['limit'];
-    $DataBaseTableName = substr($type, strpos($type, '_') + 1);
-    $sql = "SELECT `task_id` FROM $DataBaseTableName GROUP BY `task_id` ";
-    $fieldName= null;
-    
+    $start = $data['start'];
+    $end = $data['end'];
+    $sortType = $data['sortType'];
+
+    $fieldName = null;
+
     switch ($type) {
         case 'converted_CurrentTasks':
             $fieldName = 'timeCreated';
@@ -16,22 +18,43 @@ function getConvertedTasks($type, $connect, $data)
             $fieldName = 'timeCompleted';
             break;
     }
+    $DataBaseTableName = substr($type, strpos($type, '_') + 1);
 
-    $sql = $sql . "ORDER BY SUM($fieldName) DESC LIMIT $limit ";
+    $sql2 = "CREATE TABLE {$DataBaseTableName}Sort SELECT * FROM $DataBaseTableName ";
+
+    if ($start and $end) {
+        $sql2 = $sql2 . "WHERE $fieldName BETWEEN '$start 00:00:00' AND '$end 23:59:59' ";
+    }
+
+    if ($sortType === null) {
+        $sortType = 'DESC';
+    }
+
+    $sql2 = $sql2 . "ORDER BY $fieldName $sortType";
+
+
+    $result2 = mysqli_query($connect, $sql2);
+    checkDataBaseRequest($connect, $sql2);
+
+
+    $sql = "SELECT `task_id` FROM {$DataBaseTableName}Sort GROUP BY `task_id` ";
+    $sql = $sql . "LIMIT $limit ";
 
     $result = mysqli_query($connect, $sql);
     checkDataBaseRequest($connect, $sql);
     $currentTasks = mysqli_fetch_all($result, MYSQLI_ASSOC);
     $tasks = [];
 
+    mysqli_query($connect, "DROP TABLE {$DataBaseTableName}Sort");
+
     foreach ($currentTasks as $value) {
         $resultUsers = mysqli_query($connect, "SELECT `user_id`, `$fieldName` FROM $DataBaseTableName WHERE `task_id` = '$value[task_id]'");
         $users = mysqli_fetch_all($resultUsers, MYSQLI_ASSOC);
-        
+
         $ids = [];
 
         foreach ($users as $user) {
-            $ids[] =  $user['user_id'];
+            $ids[] = $user['user_id'];
         }
 
         $ids = implode(',', $ids);
@@ -41,7 +64,7 @@ function getConvertedTasks($type, $connect, $data)
         CheckDataBaseRequest($connect, $resultMovers);
 
         $Movers = mysqli_fetch_all($resultMovers, MYSQLI_ASSOC);
-        
+
         $resultTask = mysqli_query($connect, "SELECT * FROM Tasks WHERE `id` = '$value[task_id]'");
         $task = mysqli_fetch_all($resultTask, MYSQLI_ASSOC);
         $task = $task[0];
