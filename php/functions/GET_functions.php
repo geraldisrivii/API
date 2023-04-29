c12d12e27b8eb092f8cd62fac6ea38cd73f3cca3
Alexander <aleksander.freelancer@gmail.com>
2023-04-29 06:57:33 +0600

    Before refactoring

--- php/functions/GET_functions.php
+++ php/functions/GET_functions.php
@@ -3,75 +3,53 @@
 // GET
 function getConvertedTasks($type, $connect, $data)
 {
-    $start = $data['with'];
-    $end = $data['without'];
     $limit = $data['limit'];
-    $offset = $data['offset'];
-
-    $Movers_Links = [];
     $DataBaseTableName = substr($type, strpos($type, '_') + 1);
-    $sql = "SELECT * FROM $DataBaseTableName ";
-
-    $sql = $sql . "WHERE `timeCreated` BETWEEN '$start 00:00:00' AND '$end 23:59:59' ";
+    $sql = "SELECT `task_id` FROM $DataBaseTableName GROUP BY `task_id` ";
+    $fieldName= null;
     
-    $sql = $sql . "LIMIT " . ($limit + 1);
+    switch ($type) {
+        case 'converted_CurrentTasks':
+            $fieldName = 'timeCreated';
+            break;
+        case 'converted_CompletedTasks':
+            $fieldName = 'timeCompleted';
+            break;
+    }
+
+    $sql = $sql . "ORDER BY SUM($fieldName) DESC LIMIT $limit ";
 
     $result = mysqli_query($connect, $sql);
-
     checkDataBaseRequest($connect, $sql);
-
-    verifyNotNull($result, substr($DataBaseTableName, 0, -1) . " not found");
-
-    // Current Tasks - Not Converted (Just links)
-
-    $links = mysqli_fetch_all($result, MYSQLI_ASSOC);
-
-    // Value here is array - element of array - links
-
-    foreach ($links as $value) {
-        if ($Movers_Links[$value['task_id']] == null) {
-            $Movers_Links[$value['task_id']] = [$value['user_id']];
-        } else {
-            $Movers_Links[$value['task_id']][] = $value['user_id'];
-        }
-    }
-
+    $currentTasks = mysqli_fetch_all($result, MYSQLI_ASSOC);
     $tasks = [];
 
-    foreach ($Movers_Links as $key => $value) {
-        // GET task
-        $sql = "SELECT Tasks.id, Tasks.title, Tasks.price, currentTasks.timeCreated 
-        FROM Tasks 
-            LEFT JOIN currentTasks ON Tasks.id = currentTasks.task_id  
-        WHERE Tasks.id = '$key'";
+    foreach ($currentTasks as $value) {
+        $resultUsers = mysqli_query($connect, "SELECT `user_id`, `$fieldName` FROM $DataBaseTableName WHERE `task_id` = '$value[task_id]'");
+        $users = mysqli_fetch_all($resultUsers, MYSQLI_ASSOC);
+        
+        $ids = [];
 
-        $result = mysqli_query($connect, $sql);
-        checkDataBaseRequest($connect, $sql);
-        $task = mysqli_fetch_all($result, MYSQLI_ASSOC);
-        $task = $task[0];
-        // Get peoples
-        $users = [];
-        foreach ($value as $id) {
-            $sql = "SELECT * FROM Movers WHERE id = '$id'";
-
-            $result = mysqli_query($connect, $sql);
-
-            verifyNotNull($result);
-
-            $user = mysqli_fetch_all($result, MYSQLI_ASSOC);
-
-            $users[] = $user[0];
+        foreach ($users as $user) {
+            $ids[] =  $user['user_id'];
         }
 
-        // Set peoples in task
+        $ids = implode(',', $ids);
 
-        $task['peoples'] = $users;
+        $resultMovers = mysqli_query($connect, "SELECT * FROM Movers WHERE `id` IN ($ids)");
 
+        CheckDataBaseRequest($connect, $resultMovers);
+
+        $Movers = mysqli_fetch_all($resultMovers, MYSQLI_ASSOC);
+        
+        $resultTask = mysqli_query($connect, "SELECT * FROM Tasks WHERE `id` = '$value[task_id]'");
+        $task = mysqli_fetch_all($resultTask, MYSQLI_ASSOC);
+        $task = $task[0];
+        $task['peoples'] = $Movers;
+        $task['time'] = $user[$fieldName];
         $tasks[] = $task;
     }
-
     echo json_encode($tasks);
-
 }
 function getDataFromID($sql, $connect, $errorMessage = 'Data not found')
 {
